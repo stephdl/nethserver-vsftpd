12f9f35dd618d9487af9978cc11a9dcf55de057a
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2014-05-29 10:58:32 +0200

    Web UI: update translation. Refs #1762

--- root/usr/share/nethesis/NethServer/Language/en/NethServer_Module_FTP_Users.php
+++ root/usr/share/nethesis/NethServer/Language/en/NethServer_Module_FTP_Users.php
@@ -4,6 +4,6 @@ $L['Key_label'] = 'User';
 $L['Password_label'] = 'Password';
 $L['status_label'] = 'Status';
 $L['Chroot_label'] = 'Chroot user on home directory ( /var/lib/nethserver/ftp/<user> )';
-$L['ChrootDir_label'] = 'Custom chroot directory';
+$L['ChrootDir_label'] = 'Custom chroot directory (eg. /var/lib/nethserver/ibay/<name> )';
 $L['Update_User_Header'] = 'Edit user';
 $L['Create_User_Header'] = 'Create user';
--- root/usr/share/nethesis/NethServer/Language/it/NethServer_Module_FTP_Users.php
+++ root/usr/share/nethesis/NethServer/Language/it/NethServer_Module_FTP_Users.php
@@ -4,6 +4,6 @@ $L['Key_label'] = 'Utente';
 $L['Password_label'] = 'Password';
 $L['status_label'] = 'Stato';
 $L['Chroot_label'] = 'Abilita chroot utente nella home directory ( /var/lib/nethserver/ftp/<user> )';
-$L['ChrootDir_label'] = 'Directory choor personalizzata';
+$L['ChrootDir_label'] = 'Directory chroot personalizzata (es. /var/lib/nethserver/ibay/<name> )';
 $L['Update_User_Header'] = 'Modifica utente';
 $L['Create_User_Header'] = 'Crea utente';
